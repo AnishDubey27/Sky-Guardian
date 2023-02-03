2212376e05f60aa804f7260b47ef1890671d3991
Anish Dubey <87861510+AnishDubey27@users.noreply.github.com>
2023-02-03 20:41:11 +0530

    Update settings.py

--- weatherdetector/settings.py
+++ weatherdetector/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-)8)jr_o6nolzvgu(-hwbp@ndih=3)zxt3z)+mz!-7@+7jzsjw@
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [*]
 
 
 # Application definition
